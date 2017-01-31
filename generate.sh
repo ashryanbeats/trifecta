d3e2bfb0a965577ff9765038c24902b2e509c931
Ash <ash@ashryanbeats.com>
2017-01-31 14:42:55 +0900

    Initial commit.

---
+++ generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# const current dir
+readonly CURRENTDIR=$(pwd)
+
+# const script dir
+readonly SCRIPTDIR="/Users/arnwine/Scripts/trifecta/."
+
+# copy all files from script dir to current dir
+cp -R $SCRIPTDIR $CURRENTDIR
+rm "$CURRENTDIR/generate.sh"
+cd $CURRENTDIR && git init
